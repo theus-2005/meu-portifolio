3c381d657935d3c5d0714e29c2a1edb0b5ad57e7
Matheus Degrande Garcia <matheus10degrande@gmail.com>
2025-07-21 19:35:31 -0300

    Atualizações em package.json e package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/meu-portifolio/',
   plugins: [react(), tailwindcss()]
 })
